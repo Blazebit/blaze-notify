ae60f1c5fae27b6bcd240915ee11bf5b714965ec
Christian Beikov <christian.beikov@gmail.com>
2018-11-13 23:56:15 +0100

    Add travis ci config

---
+++ .travis-deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ "$TRAVIS_REPO_SLUG" == "Blazebit/blaze-notify" ] &&
+    [ "$TRAVIS_BRANCH" == "master" ] &&
+    [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+
+  echo "Starting snapshot deployment..."
+  #mvn -s .travis-settings.xml -DperformRelease -DskipTests -Dgpg.skip=true -Dquiet=true deploy
+  echo "Snapshots deployed!"
+
+else
+  echo "Skipping snapshot deployment..."
+fi
