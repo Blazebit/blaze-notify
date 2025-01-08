163323d14b5c8d58844300ff6c498841b9cafd8a
Moritz Becker <moritz.becker@gmx.at>
2025-01-08 14:49:53 +0100

    Update license headers

--- core/api/src/main/java/com/blazebit/notify/NotificationMessageResolverFactory.java
+++ core/api/src/main/java/com/blazebit/notify/NotificationMessageResolverFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 - 2023 Blazebit.
+ * Copyright 2018 - 2025 Blazebit.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
